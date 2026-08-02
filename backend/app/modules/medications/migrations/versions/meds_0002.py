9b20443be117185eda5a0db126cfb06e7058c603
lamanji <lamin0733@gmail.com>
2026-08-02 03:25:43 +0100

    Upgrade Node to 22, fix vulnerabilities, update i18n, add dummy meds_0002 migration

---
+++ backend/app/modules/medications/migrations/versions/meds_0002.py
@@ -0,0 +1,20 @@
+"""empty migration – placeholder for missing meds_0002
+
+Revision ID: meds_0002
+Revises: meds_0001
+Create Date: 2024-01-01 00:00:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+revision = 'meds_0002'
+down_revision = 'meds_0001'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
